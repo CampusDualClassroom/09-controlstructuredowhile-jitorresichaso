962f543f03ae280197d6dc4feb172877d5d2b1e7
Juan Ignacio Torres Ichaso <jitorresichaso@macrotest.es>
2025-10-26 13:58:20 +0100

    Exercise 9 solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -6,11 +6,11 @@ public class Exercise {
         doWhileLoop(10);
     }
 
-    // Comprueba el funcionamiento del bucle do-while, que para cada interacción muestre el siguiente mensaje:
-    // Entra con valor: X, sale con valor: X+1
-    // Entra con valor: X+1, sale con valor: X+2;
-    // etc.
     public static void doWhileLoop(int num) {
-
+        int x = 0;
+        do {
+            System.out.println("Entra con valor: " + x + ", sale con valor: " + (x + 1));
+            x++;
+        } while (x < num);
     }
 }
